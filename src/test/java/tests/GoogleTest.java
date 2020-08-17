7d3da6a5fb717b7a8ef7315c7dea10ce4fb30df7
Станислав <stanislavkovalev.m@gmail.com>
2020-08-17 22:16:49 +0300

    Initial commit

---
+++ src/test/java/tests/GoogleTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.SendKeysAction;
+
+import java.sql.DriverManager;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
+class GoogleTest {
+    @Test
+    void selenideSearchTest() {
+        // Открыть google
+        open("https://google.com");
+
+        // Ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(text("ru.selenide.org"));
+    }
+}
